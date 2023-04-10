a3015c42ecb52a9225c14b8947a8b88bdf0f0e78
Sergei Palmer <10038681+s-palmer@users.noreply.github.com>
2023-04-10 18:35:13 +0100

    Remove {} from Layout

--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "react";
 
-export const PageLayout = (props: PropsWithChildren<{}>) => {
+export const PageLayout = (props: PropsWithChildren) => {
 
   return (
     <main className="flex justify-center h-screen">
