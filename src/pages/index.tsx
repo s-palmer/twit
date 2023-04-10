47b6bc9edaedf6215130bf7e25b4ca10ed9a8e82
Sergei Palmer <10038681+s-palmer@users.noreply.github.com>
2023-04-10 13:17:32 +0100

    Loading component, small refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,51 @@ import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
+import { LoadingPage } from "~/components/Loading/loading";
 dayjs.extend(relativeTime);
 
+type PostWithUser = RouterOutputs["posts"]["getAll"][number]
+
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props
+  return <div className="flex flex-col">
+    <div key={post.id} className="p-4 gap-3 border-b border-slate-400 flex">
+      <Image src={author.profileImageUrl} alt={`@${author.username} Profile Picture"`} className="w-14 h-14 rounded-full" width={56} height={56} />
+      <div className="flex flex-col">
+        <div className="flex text-slate-300 gap-1">
+          <span>{`@${author.username}`}</span>
+          <span className="font-thin">{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+        </div>
+        <span>{post.content}</span>
+      </div>
+    </div>
+  </div>
+}
+
+const Feed = () => {
+  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
+
+  if (postsLoading) return <LoadingPage />
+
+  if (!data) return <div>Something went wrong</div>
+
+  return (
+    <div className="flex flex-col">
+      {[...data, ...data]?.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  )
+}
+
 const Home: NextPage = () => {
-  const user = useUser();
+  const { isLoaded: userLoaded, isSignedIn } = useUser();
 
-  const { data, isLoading } = api.posts.getAll.useQuery();
+  // Start fetching asap
+  api.posts.getAll.useQuery();
 
-  if (isLoading) return <div>Loading...</div>;
-
-  if (!data) return <div>Something went wrong</div>;
+  // Return empty div if user isn't loaded
+  if (!userLoaded) return <div />
 
   const CreatePostWizard = () => {
     const { user } = useUser();
@@ -26,25 +61,6 @@ const Home: NextPage = () => {
     return <div className="flex gap-3 w-full">
       <Image src={user.profileImageUrl} alt="Profile Image" className="w-14 h-14 rounded-full" width={56} height={56} />
       <input type="text" placeholder="Add a twit" className="bg-transparent grow outline-none" />
-    </div>
-  }
-
-  type PostWithUser = RouterOutputs["posts"]["getAll"][number]
-
-
-  const PostView = (props: PostWithUser) => {
-    const { post, author } = props
-    return <div className="flex flex-col">
-      <div key={post.id} className="p-4 gap-3 border-b border-slate-400 flex">
-        <Image src={author.profileImageUrl} alt={`@${author.username} Profile Picture"`} className="w-14 h-14 rounded-full" width={56} height={56} />
-        <div className="flex flex-col">
-          <div className="flex text-slate-300 gap-1">
-            <span>{`@${author.username}`}</span>
-            <span className="font-thin">{`· ${dayjs(post.createdAt).fromNow()}`}</span>
-          </div>
-          <span>{post.content}</span>
-        </div>
-      </div>
     </div>
   }
 
@@ -58,22 +74,18 @@ const Home: NextPage = () => {
       <main className="flex justify-center h-screen">
         <div className="border-slate-400 h-full w-full md:max-w-2xl border-x">
           <div className="border-b border-slate-400 p-4">
-            {!user.isSignedIn && (
+            {!isSignedIn && (
               <div className="flex justify-center">]
                 <SignInButton redirectUrl="/">
                   Sign in
                 </SignInButton>
               </div>
             )}
-            {!!user.isSignedIn &&
+            {!!isSignedIn &&
               <CreatePostWizard />
             }
           </div>
-          <div className="flex flex-col">
-            {[...data, ...data]?.map((fullPost) => (
-              <PostView {...fullPost} key={fullPost.post.id} />
-            ))}
-          </div>
+          <Feed />
         </div>
       </main>
     </>
